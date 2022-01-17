e3b727b3302c1dcba709b65b0861692eb45503af
ngchinhow <ngchinhow@gmail.com>
2022-01-17 14:15:10 +0800

    Connected to redis for caching. Prettified result page

--- src/main/java/com/tfip2021/module2/service/Constants.java
+++ src/main/java/com/tfip2021/module2/service/Constants.java
@@ -2,5 +2,6 @@ package com.tfip2021.module2.service;
 
 public class Constants {
     public static final String BASE_URI = "https://api.openweathermap.org/data/2.5";
-    public static final String API_KEY = "OPENWEATHERMAP_API_KEY";
+    public static final String ENV_API_KEY = "OPENWEATHERMAP_API_KEY";
+    public static final String ENV_REDIS_PASSWORD = "REDIS_PASSWORD";
 }
--- src/main/java/com/tfip2021/module2/service/WeatherService.java
+++ src/main/java/com/tfip2021/module2/service/WeatherService.java
@@ -4,6 +4,8 @@ import java.io.StringReader;
 
 import com.tfip2021.module2.model.City;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +20,47 @@ import static com.tfip2021.module2.service.Constants.*;
 
 @Service
 public class WeatherService {
+    private static final String REDIS_CITY_LAST_UPDATED = "city_last_updated";
+    private static final String REDIS_CITY_WEATHER = "city_weather";
+    private static final long REFRESH_TIME = 1800;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
     public City getWeather(String q) {
-        final String apiKey = System.getenv(API_KEY);
+        String cleanedQuery = q.trim().toLowerCase().
+            replaceAll("\\p{M}", "").replaceAll(" ", "+");
+        String lastUpdatedAt = (String) redisTemplate.opsForHash().
+            get(REDIS_CITY_LAST_UPDATED, cleanedQuery);
+        String weather;
+        if (
+            lastUpdatedAt != null &&
+            Long.parseLong(lastUpdatedAt) + REFRESH_TIME > 
+                System.currentTimeMillis() / 1000L
+        ) {
+            System.out.println("did not do a refresh");
+            weather = getWeatherFromRedis(cleanedQuery);
+        } else {
+            System.out.println("did a refresh");
+            weather = getWeatherFromAPI(cleanedQuery);
+        }
+        JsonReader reader = Json.createReader(new StringReader(weather));
+        return City.fromJson(reader.readObject());
+    }
+
+    private String getWeatherFromRedis(String query) {
+        return (String) redisTemplate.opsForHash().get(REDIS_CITY_WEATHER, query);
+    }
+
+    private String getWeatherFromAPI(String q) {
+        System.out.println(q);
+        final String weatherAPIKey = System.getenv(ENV_API_KEY);
         String url = UriComponentsBuilder.
             fromUriString(BASE_URI).
             pathSegment("weather").
             queryParam("q", q).
-            queryParam("appid", apiKey).
+            queryParam("appid", weatherAPIKey).
+            encode().
             toUriString();
         System.out.println(url);
         RequestEntity<Void> req = RequestEntity.
@@ -33,7 +69,17 @@ public class WeatherService {
             build();
         RestTemplate template = new RestTemplate();
         ResponseEntity<String> resp = template.exchange(req, String.class);
-        JsonReader reader = Json.createReader(new StringReader(resp.getBody()));
-        return City.fromJson(reader.readObject());
+        String updatedAt = String.valueOf(System.currentTimeMillis() / 1000L);
+        redisTemplate.opsForHash().put(
+            REDIS_CITY_LAST_UPDATED,
+            q,
+            updatedAt
+        );
+        redisTemplate.opsForHash().put(
+            REDIS_CITY_WEATHER,
+            q,
+            resp.getBody()
+        );
+        return resp.getBody();
     }
 }
